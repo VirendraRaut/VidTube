87a712d998c3df29a5293a80e16afa2cbcdd8c61
VirendraRaut <virendraraut1306@gmail.com>
2025-10-25 11:55:32 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import dotenv from "dotenv"
 import { app } from "./app.js";
+
+dotenv.config();
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log("Server is running on port ${port}");
+    console.log(`Server is running on => http://localhost:${port}`);
 })
