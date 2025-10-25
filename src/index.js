9a032cbd98e235f4899e55a802c602817fa70b8d
VirendraRaut <virendraraut1306@gmail.com>
2025-10-25 12:01:55 +0530

    PORT

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import dotenv from "dotenv"
 import { app } from "./app.js";
 
-dotenv.config();
+dotenv.config({
+    path: './.env'
+});
 
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Server is running on => http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on => http://localhost:${PORT}`);
 })
