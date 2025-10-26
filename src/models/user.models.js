9d681b9dc89c7b4020e8293b36d94f230cc96d8a
VirendraRaut <virendraraut1306@gmail.com>
2025-10-27 01:32:05 +0530

    commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = mongoose.Schema({
     username: { type: String, required: true, lowercase: true, unique: true, trim: true },
@@ -21,8 +22,13 @@ const userSchema = mongoose.Schema({
 })
 
 userSchema.pre("save", async function (next) {
-    
+    if (!this.modified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
     next()
 })
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model("User", userSchema)
