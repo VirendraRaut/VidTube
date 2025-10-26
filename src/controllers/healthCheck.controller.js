f52932de85fac8c356fb466176af7d3982d6d20b
VirendraRaut <virendraraut1306@gmail.com>
2025-10-26 16:10:23 +0530

    comit

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,3 +1,7 @@
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+
+const healthCheck = async (req, res) => {
+    
+}
