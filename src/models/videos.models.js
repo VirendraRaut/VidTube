9ced0fa663f3dcc016b9962ed074e83a119f2f68
VirendraRaut <virendraraut1306@gmail.com>
2025-10-27 00:04:28 +0530

    video

--- src/models/videos.models.js
+++ src/models/videos.models.js
@@ -1,7 +1,30 @@
 import mongoose, { Schema } from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-
-})
+    videoFile: {
+        type: String, required: true
+    },
+    thumbnail: {
+        type: String, required: true
+    },
+    title: {
+        type: String
+    },
+    description: {
+        type: String, required: true
+    },
+    views: {
+        type: Number, default: 0
+    },
+    duration: {
+        type: Number
+    },
+    isPublish: {
+        type: Number, default: true
+    },
+    ownser: {
+        type: Schema.Types.ObjectId, ref: "User"
+    },
+}, { timeseries: true })
 
 export const Video = mongoose.model('Video', videoSchema)
