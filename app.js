7c550946d9082b692d2de4abd497e8b9a356bc29
VirendraRaut <virendraraut1306@gmail.com>
2025-10-26 16:18:30 +0530

    healthCheckRouter router

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors";
+import healthCheckRouter from "./src/routes/healthCheck.route.js";
 
 const app = express();
 
@@ -14,5 +15,8 @@ app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
 app.use(express.static("public"))
+
+// routes
+app.use("/api/v1/health-check", healthCheckRouter)
 
 export { app };
