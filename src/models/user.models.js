7d2cd04cd5bba7d713706a76df7676dfdf265356
VirendraRaut <virendraraut1306@gmail.com>
2025-10-26 16:39:00 +0530

    commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,12 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = mongoose.Schema({
     username: { type: String, required: true, lowercase: true, unique: true, trim: true },
     fullName: { type: String, required: true },
     email: { type: String, required: true, trim: true },
-    pasword: { type: String, required: true, trim: true },
-    avatar: {type : String},
-    coverImage: {type : String}
+    password: { type: String, required: true, trim: true },
+    avatar: { type: String },
+    coverImage: { type: String },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    refreshToken: {
+        type: String,
+    }
+}, {
+    timestamps: true,
 })
 
 export const User = mongoose.model("User", userSchema)
