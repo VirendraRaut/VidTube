c71626aa67f4c52917f7951f9b8b7265faf8a58f
VirendraRaut <virendraraut1306@gmail.com>
2025-10-27 01:49:56 +0530

    commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import healthCheckRouter from "./src/routes/healthCheck.route.js";
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
+
+app.use(cookieParser())
 
 app.use(express.json({ limit: '16kb' }))
 
