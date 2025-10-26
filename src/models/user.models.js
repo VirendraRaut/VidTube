148dd4b468785d62c5040ef31b330f88ae67c881
VirendraRaut <virendraraut1306@gmail.com>
2025-10-27 01:42:42 +0530

    commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -49,8 +49,6 @@ userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-      email: this.email,
-      username: this.username,
     },
     process.env.REFRESH_TOKEN_SECRET,
     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
