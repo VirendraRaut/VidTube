2707ffaf629083ea5f4a9be045f4aefa07d6454a
VirendraRaut <virendraraut1306@gmail.com>
2025-10-26 16:29:33 +0530

    user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    pasword: { type: String, required: true }
 })
 
-export const User = mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema)
