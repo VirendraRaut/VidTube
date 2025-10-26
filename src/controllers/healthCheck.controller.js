22b67fb6a3dd37d3bab75e221744e321a538cce6
VirendraRaut <virendraraut1306@gmail.com>
2025-10-26 16:12:39 +0530

    commit

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -2,6 +2,6 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const healthCheck = async (req, res) => {
-    
-}
+const healthCheck = asyncHandler((req, res, next)=>{
+    return res.status(200).json(ApiResponse(200, "Health-check is fine..."))
+})
