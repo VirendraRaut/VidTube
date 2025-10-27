bec3719ebaa87ec66496d6a9cee4412456cfae12
VirendraRaut <virendraraut1306@gmail.com>
2025-10-28 00:27:29 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -40,8 +40,12 @@ const userRegister = asyncHandler(async (req, res) => {
             throw new ApiError(400, "Failed to register user")
         }
 
+        await createdUser.save();
+
+        return new ApiResponse(200, "User registered successfully", { createdUser })
+
     } catch (error) {
-        throw new ApiError(400, "Failed to register user")
+        throw new ApiError(400, "Failed to register user", error)
     }
 })
 
