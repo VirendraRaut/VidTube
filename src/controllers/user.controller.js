63be85f34c34d2d24609466b1dc6f666e7a39897
VirendraRaut <virendraraut1306@gmail.com>
2025-10-27 23:49:51 +0530

    commit

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { ApiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const userRegister = asyncHandler(async (req, res) => {
+    try {
+        
+    } catch (error) {
+        ApiError()
+    }
+})
