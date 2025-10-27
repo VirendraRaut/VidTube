2827005d8538fc9a8001a8ed271f77a64d2c1f40
VirendraRaut <virendraraut1306@gmail.com>
2025-10-28 00:06:20 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,22 @@
-import { ApiError } from "../utils/apiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const userRegister = asyncHandler(async (req, res) => {
     try {
+        const { fullName, email, username, password, } = req.body;
 
+        // validation
+        if (!fullName || !email || !username || password) {
+            throw new ApiError(400, "All fields are mandatory")
+        }
+
+        const user = await User.findOne({ email });
+        if(user){
+            throw new ApiError(400, "User exists already")
+        }
     } catch (error) {
-        ApiError()
+        throw new ApiError(400, "Failed to register user")
     }
 })
 
