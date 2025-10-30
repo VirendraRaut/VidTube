128ae39b8e451fd7e46d7359ac354f56458680bf
VirendraRaut <virendraraut1306@gmail.com>
2025-10-30 20:04:28 +0530

    commit

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,6 +4,9 @@ import { User } from "../models/user.models";
 export const verifyJWT = async (req, _, next) => {
     try {
         const token = await req.cookies.accessToken || req.headers("Authorization")?.replace("Bearer ", "");
+         if (!token) {
+            throw new ApiError(400, "Unauthorized access");
+        }
     } catch (error) {
         console.log("", error);
         return res.status(500).json({ success: false, message: "" })
