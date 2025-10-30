4d1dd0ccc5467a5195498b47ef1883f03305ce82
VirendraRaut <virendraraut1306@gmail.com>
2025-10-30 21:06:40 +0530

    commit

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.models";
+import { User } from "../models/user.models.js";
 
 export const verifyJWT = async (req, _, next) => {
     try {
