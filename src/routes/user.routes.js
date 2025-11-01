b971b6bfc726e2effea1d992abe16b32a8828b8a
VirendraRaut <virendraraut1306@gmail.com>
2025-11-01 18:02:23 +0530

    commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { changeCurrentPassword, getCurrentUser, loginUser, logout, refreshAccessToken, updateAccountDetails, updateAvatar, updateCoverImage, userRegister } from "../controllers/user.controller.js"
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, loginUser, logout, refreshAccessToken, updateAccountDetails, updateAvatar, updateCoverImage, userRegister } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.js";
 
@@ -21,6 +21,7 @@ userRouter.post("/refresh-token", refreshAccessToken);
 userRouter.post("/logout", verifyJWT, logout);
 userRouter.post("/curret-password", verifyJWT, changeCurrentPassword);
 userRouter.get("/curret", verifyJWT, getCurrentUser);
+userRouter.get("/channel/:username", verifyJWT, getUserChannelProfile);
 userRouter.post("/update-account", verifyJWT, updateAccountDetails);
 userRouter.post("/change-avatar", verifyJWT, updateAvatar);
 userRouter.post("/change-coverImae", verifyJWT, updateCoverImage);
